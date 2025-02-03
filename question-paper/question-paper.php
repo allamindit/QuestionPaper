0b60e02908e10cb71ea6ecddb30e0fdde6ab3710
All amin sarker <67311973+allamindit@users.noreply.github.com>
2025-02-03 21:58:56 +0600

    Add files via upload

---
+++ question-paper/question-paper.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:       Question Paper
+ * Plugin URI:        https://wordpress.org/plugins/question-paper/
+ * Description:       Question Paper plugin will help you to enable create new user roll and make custom type post system with Subject and Lesson system for create Question Paper to your WordPress School or college website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Al amin sarker
+ * Author URI:        https://github.com/allamindit
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       qpaper
+ */
+
+
+?>
